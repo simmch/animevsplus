28b5dc723b1255a91f25032641c9240dea5aa226
RocBambino <83316065+RocBambino@users.noreply.github.com>
2023-01-09 23:55:10 -0500

    heroku

--- client/src/UTIL/config.js
+++ client/src/UTIL/config.js
@@ -1,3 +1,3 @@
-export const AUTH = "https://discord.com/api/oauth2/authorize?client_id=861296553704816671&redirect_uri=https%3A%2F%2Fcrowndashboard.herokuapp.com%2Fauth%2Fredirect&response_type=code&scope=identify%20email%20guilds"
+//export const AUTH = "https://discord.com/api/oauth2/authorize?client_id=861296553704816671&redirect_uri=https%3A%2F%2Fcrowndashboard.herokuapp.com%2Fauth%2Fredirect&response_type=code&scope=identify%20email%20guilds"
 // TEST 
-// export const AUTH = "https://discord.com/api/oauth2/authorize?client_id=861296553704816671&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fauth%2Fredirect&response_type=code&scope=email%20guilds"
+export const AUTH = "https://discord.com/api/oauth2/authorize?client_id=861296553704816671&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fauth%2Fredirect&response_type=code&scope=email%20guilds"
