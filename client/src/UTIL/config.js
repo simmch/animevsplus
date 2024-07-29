d3ef2f2a8f760a276e680bcfb725bbe53b47e3c8
christopher simmons <kadensdad93@gmail.com>
2024-07-28 18:03:55 -0700

    updated redirect url in the right bot

--- client/src/UTIL/config.js
+++ client/src/UTIL/config.js
@@ -1,3 +1,3 @@
-export const AUTH = "https://discord.com/api/oauth2/authorize?client_id=1062227127199612948&redirect_uri=https%3A%2F%2Fanime-vs-dashboard-build-env.eba-8gexeen5.us-east-2.elasticbeanstalk.com/%2Fauth%2Fredirect&response_type=code&scope=identify%20email%20guilds"
+export const AUTH = "https://discord.com/api/oauth2/authorize?client_id=1062227127199612948&redirect_uri=http%3A%2F%2Fanime-vs-dashboard-build-env.eba-8gexeen5.us-east-2.elasticbeanstalk.com/%2Fauth%2Fredirect&response_type=code&scope=identify%20email%20guilds"
 // TEST 
 // export const AUTH = "https://discord.com/api/oauth2/authorize?client_id=1062227127199612948&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fauth%2Fredirect&response_type=code&scope=identify%20email%20guilds"
